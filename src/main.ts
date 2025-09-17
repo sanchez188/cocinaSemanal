e917a9cc62961dce50bdc77d157f962667174402
Javier Sánchez <javier.sanchez@ayahealthcare.com>
2025-09-17 11:17:01 -0600

    mejoras

--- src/main.ts
+++ src/main.ts
@@ -167,9 +167,5 @@ bootstrapApplication(App, {
       enabled: !isDevMode(),
       registrationStrategy: "registerWhenStable:30000",
     }),
-    provideServiceWorker("ngsw-worker.js", {
-      enabled: !isDevMode(),
-      registrationStrategy: "registerWhenStable:30000",
-    }),
   ],
 });
